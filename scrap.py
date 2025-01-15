5c5e05db2efba85c89f5ff980c4aed31e6971504
Virat Gupta <viratgupta1072@gmail.com>
2025-01-15 20:27:05 +0530

    add big data proxy

--- scrap.py
+++ scrap.py
@@ -1,18 +1,51 @@
-import selenium.webdriver as webdriver
-from selenium.webdriver.chrome.service import Service
-import time
+# import selenium.webdriver as webdriver
+# from selenium.webdriver.chrome.service import Service
+# import time
+from selenium.webdriver import Remote, ChromeOptions
+from selenium.webdriver.chromium.remote_connection import ChromiumRemoteConnection
+from selenium.webdriver.common.by import By
 
 def scrape_website(website):
   print("Launching browser...")
-  chrome_driver_path = "./chromedriver.exe"
-  options = webdriver.ChromeOptions()
-  driver = webdriver.Chrome(service=Service(chrome_driver_path),options = options)
+  # chrome_driver_path = "./chromedriver.exe"
+  # options = webdriver.ChromeOptions()
+  # driver = webdriver.Chrome(service=Service(chrome_driver_path),options = options)
 
-  try :
-    driver.get(website)
-    print("Page loaded...")
-    html = driver.page_source
-    time.sleep(10)
-    return html
-  finally:
-    driver.quit()
+  # try :
+  #   driver.get(website)
+  #   print("Page loaded...")
+  #   html = driver.page_source
+  #   time.sleep(10)
+  #   return html
+  # finally:
+  #   driver.quit()
+  AUTH = 'brd-customer-hl_74fc088c-zone-first_scrap:ea13gt7ollpt'
+  SBR_WEBDRIVER = f'https://{AUTH}@brd.superproxy.io:9515'
+  def main():
+    print('Connecting to Scraping Browser...')
+    sbr_connection = ChromiumRemoteConnection(SBR_WEBDRIVER, 'goog', 'chrome')
+    with Remote(sbr_connection, options=ChromeOptions()) as driver:
+      print('Connected! Navigating...')
+      driver.get(website)
+      print('Taking page screenshot to file page.png')
+      driver.get_screenshot_as_file('./page.png')
+      print('Navigated! Scraping page content...')
+      html = driver.page_source
+      return html
+
+
+# AUTH = 'brd-customer-hl_74fc088c-zone-first_scrap:ea13gt7ollpt'
+# SBR_WEBDRIVER = f'https://{AUTH}@brd.superproxy.io:9515'
+# def main():
+#     print('Connecting to Scraping Browser...')
+#     sbr_connection = ChromiumRemoteConnection(SBR_WEBDRIVER, 'goog', 'chrome')
+#     with Remote(sbr_connection, options=ChromeOptions()) as driver:
+#         print('Connected! Navigating...')
+#         driver.get('https://example.com')
+#         print('Taking page screenshot to file page.png')
+#         driver.get_screenshot_as_file('./page.png')
+#         print('Navigated! Scraping page content...')
+#         html = driver.page_source
+#         print(html)
+# if __name__ == '__main__':
+#   main()
