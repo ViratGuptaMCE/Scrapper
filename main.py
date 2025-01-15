ae7a55e7f3118ff1e527442157e093d61d2d7523
Virat Gupta <viratgupta1072@gmail.com>
2025-01-15 21:47:52 +0530

    get done with UI

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from scrap import scrape_website
+from scrap import scrape_website, split_dom_content , clean_body_cont,extract_body_content
 
 st.title("Data Scraper")
 url = st.text_input("Enter WebLink : ")
@@ -7,4 +7,19 @@ url = st.text_input("Enter WebLink : ")
 if st.button("Scrape Site"):
   st.write("Scraping the website")
   result = scrape_website(url)
-  print(result)
+  # print(result)
+  body_cont = extract_body_content(result)
+  cleaned_cont = clean_body_cont(body_cont)
+  st.session_state.dom_content = cleaned_cont
+
+  with st.expander("View DOM Content"):
+    st.text_area("DOM Content",cleaned_cont,height=300)
+
+if "dom_content" in st.session_state:
+  parse_description = st.text_area("Describe What you want to parse")
+
+  if st.button("Parse Content"):
+    if parse_description:
+      st.write("Parsing the content")
+
+      dom_chunks = split_dom_content(st.session_state.dom_content)
--- scrap.py
+++ scrap.py
@@ -4,7 +4,10 @@
 from selenium.webdriver import Remote, ChromeOptions
 from selenium.webdriver.chromium.remote_connection import ChromiumRemoteConnection
 from selenium.webdriver.common.by import By
+from bs4 import BeautifulSoup
 
+AUTH = 'brd-customer-hl_74fc088c-zone-first_scrap:ea13gt7ollpt'
+SBR_WEBDRIVER = f'https://{AUTH}@brd.superproxy.io:9515'
 def scrape_website(website):
   print("Launching browser...")
   # chrome_driver_path = "./chromedriver.exe"
@@ -19,19 +22,18 @@ def scrape_website(website):
   #   return html
   # finally:
   #   driver.quit()
-  AUTH = 'brd-customer-hl_74fc088c-zone-first_scrap:ea13gt7ollpt'
-  SBR_WEBDRIVER = f'https://{AUTH}@brd.superproxy.io:9515'
-  def main():
-    print('Connecting to Scraping Browser...')
-    sbr_connection = ChromiumRemoteConnection(SBR_WEBDRIVER, 'goog', 'chrome')
-    with Remote(sbr_connection, options=ChromeOptions()) as driver:
-      print('Connected! Navigating...')
-      driver.get(website)
-      print('Taking page screenshot to file page.png')
-      driver.get_screenshot_as_file('./page.png')
-      print('Navigated! Scraping page content...')
-      html = driver.page_source
-      return html
+  sbr_connection = ChromiumRemoteConnection(SBR_WEBDRIVER, 'google', 'chrome')
+  with Remote(sbr_connection, options=ChromeOptions()) as driver:
+    print('Connected! Navigating...')
+    driver.get(website)
+    print('Taking page screenshot to file page.png')
+    driver.get_screenshot_as_file('./page.png')
+    print('Navigated! Scraping page content...')
+    html = driver.page_source
+    return html
+# if __name__ == '__main__':
+#   main()  
+  
 
 
 # AUTH = 'brd-customer-hl_74fc088c-zone-first_scrap:ea13gt7ollpt'
@@ -49,3 +51,26 @@ def scrape_website(website):
 #         print(html)
 # if __name__ == '__main__':
 #   main()
+
+def extract_body_content(html_content):
+  soup = BeautifulSoup(html_content,"html.parser")
+  body_content = soup.body
+  if body_content : 
+    return str(body_content)
+  return ""
+
+def clean_body_cont(body_content):
+  soup = BeautifulSoup(body_content,"html.parser")
+
+  for script_or_style in soup(["script","style"]):
+    script_or_style.extract()
+
+  cleaned_content = soup.get_text(separator="\n")
+  cleaned_content = "\n".join(line.strip() for line in cleaned_content.splitlines() if line.strip())
+
+  return cleaned_content
+
+def split_dom_content(dom_content,max_length = 6000):
+  return [
+    dom_content[i: i+ max_length] for i in range(0,len(dom_content),max_length)
+  ]
